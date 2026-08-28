d351ecafdecd286645ec479bf068036d8a97c75c
Keshav <142299779+KshavCode@users.noreply.github.com>
2026-08-28 14:41:36 +0530

    fix: Notice Board, Scraper and less error-prone

--- constants/links.js
+++ constants/links.js
@@ -2,6 +2,7 @@ export const NOTICES_URL = 'https://arsdcollege.ac.in/announcements.aspx'
 export const CHANGELOG_URL = "https://github.com/KshavCode/arsd-saathi-app/blob/master/CHANGELOG.md"
 export const GENERATE_PASSWORD_URL = "https://www.arsdcollege.in/Internet/Student/Login_Password_Generator.aspx"
 export const REPOSITORY_URL = "https://github.com/KshavCode/arsd-saathi-app"
+export const REMOTE_MANIFEST_URL = "https://raw.githubusercontent.com/Kshav005/arsd-saathi-json-data/refs/heads/main/scraper/manifest.json";
 
 export const FAQ_URL = "https://raw.githubusercontent.com/Kshav005/arsd-saathi-json-data/main/faq.json";
 export const DEV_MESSAGE_URL = "https://raw.githubusercontent.com/Kshav005/arsd-saathi-json-data/main/message.json";
